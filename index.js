792fc46353289d3a747f3bc48ff60b13bf54770a
Wasif Hasan <wasifhasancse@gmail.com>
2026-02-25 00:28:37 +0600

    solve 1 number problem finding the largest number in an array

---
+++ index.js
@@ -0,0 +1,15 @@
+// 1. Write a function named findMax that takes an array of numbers and returns the largest number from the array.
+
+function findMax(arr) {
+  if (arr.length === 0) {
+    return null;
+  }
+  let max = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  return max;
+}
+console.log(findMax([3, 5, 7, 2, 8]));
